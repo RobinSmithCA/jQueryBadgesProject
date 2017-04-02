126f625d7f92fb83cde992fc0655b0468fd9f5a1
Robin R. Smith-Gilbert <robin@computingdreams.com>
2017-04-01 22:50:06 -0700

    Updated per build instructions

--- assets/main.js
+++ assets/main.js
@@ -2,4 +2,19 @@ $(function() {
 
   // your code will go here
 
+   $.ajax({
+      url : 'https://www.codeschool.com/users/RobinSmith.json',
+      dataType: 'jsonp',
+      success: function( response ) {
+            $.each( response.courses.completed, function( index, course ) {
+               let entry = $( '<div class="course"></div>' );
+
+               $( '<h3>' + course.title + '</h3>' ).appendTo( entry );
+               $( '<img src="' + course.badge + '"/>' ).appendTo( entry );
+               $( '<a href="' + course.url + '" target="_blank" class="btn btn-primary">See Course</a>' ).appendTo( entry );
+
+               $( entry ).appendTo( $('#badges') );
+            } );
+         }
+   });
 });
